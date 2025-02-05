3d7064c1ec1adeb8b08fc42cec7ad7ee1b5d147f
Andreas <andreas.boden@gmail.com>
2025-02-05 16:46:08 +0000

    Modified streamlit test file

--- streamlit_file_converter.py
+++ streamlit_file_converter.py
@@ -9,7 +9,7 @@ def convert_file(uploaded_file, output_format):
     return BytesIO(converted_data), output_filename
 
 # Streamlit UI
-st.title("File Converter")
+st.title("MIC - FLIM file Converter")
 
 uploaded_file = st.file_uploader("Upload a file to convert", type=None)
 output_format = st.selectbox("Select output format", ["txt", "csv", "pdf", "json"])  # Modify as needed
